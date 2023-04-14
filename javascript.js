4e0680d1839e2b309f30bf763823fe9ee31e8bb3
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-04-14 18:35:43 -0400

    Tried to make some changes with image expansion

--- javascript.js
+++ javascript.js
@@ -6,7 +6,41 @@ const imagesParent = document.querySelectorAll('.image-box');
 inputFieled.addEventListener('keyup', (event) => {
 const userInput = event.target.value.toLowerCase();
 
-imagesParent.forEach(element => {
-  element.querySelector('p').textContent.toLowerCase().includes(userInput) ? element.style.display = 'block' : element.style.display = 'none';
-  })
+imagesParent.forEach((element) => {
+  if (element.querySelector('p').textContent.toLowerCase().includes(userInput) === true) 
+  {
+    element.style.display = 'block';
+    element.addEventListener('click', () => {
+      element.classList.add('selected');
+    });
+  }
+  else {
+    element.style.display = 'none'
+  }
+
+  if (userInput === '') {
+    element.classList.remove('selected');
+  }
 })
+
+
+});
+
+
+
+
+
+// imagesParent.forEach(element => {
+//   element.querySelector('p').textContent.toLowerCase().includes(userInput) ? element.style.display = 'block' : element.style.display = 'none';
+//   })
+
+
+// imagesParent.forEach((element) => {
+//   if (element.querySelector('p').textContent.toLowerCase().includes(userInput)) 
+//   {
+//     element.style.display = 'block';
+//     element.classList.add('selected');
+//   } else {
+//     element.style.display = 'none'
+//   }
+// })
